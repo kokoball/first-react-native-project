b26ddcc5873bb04990e209672def592fd1d76e08
kokoball <kbp4154@naver.com>
2024-05-06 23:58:59 +0900

    setting

--- android/app/src/main/java/com/fooddeliveryapp/MainActivity.java
+++ android/app/src/main/java/com/fooddeliveryapp/MainActivity.java
@@ -1,4 +1,5 @@
 package com.fooddeliveryapp;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -14,6 +15,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "FoodDeliveryApp";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
